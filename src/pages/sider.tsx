fe65e7249fb2dfa9e5bdc94c30efffa0a03412c4
abandon888 <1876746719@qq.com>
2023-10-15 21:49:20 +0800

    feat: import tailwind

---
+++ src/pages/sider.tsx
@@ -0,0 +1,11 @@
+export default function Sider() {
+  return (
+    <>
+      <div>
+        <div className="sider-btn">w</div>
+        <div className="sider-btn">s</div>
+        <div className="sider-btn">t</div>
+      </div>
+    </>
+  )
+}
